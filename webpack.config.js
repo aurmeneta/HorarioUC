4c1620fd22396e54e853afc10399cf767e424c10
Andrés Urmeneta <andresub009@gmail.com>
2021-05-17 21:25:27 -0400

    fix deploy sin index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
     entry: "./src/index.js",
@@ -30,5 +32,11 @@ module.exports = {
         publicPath: "http://localhost:3000/dist/",
         hotOnly: true
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'public'}
+            ]
+        })]
 }
