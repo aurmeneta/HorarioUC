eb2eb804eb2c872b3424c0b7004192cbaba31ea6
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-01-02 16:10:17 -0300

    chore: CRLF a LF

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020, Andrés Urmeneta B. <aurmeneta@uc.cl>
+Copyright (c) 2021, Andrés Urmeneta B. <aurmeneta@uc.cl>
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
 copyright notice and this permission notice appear in all copies.
