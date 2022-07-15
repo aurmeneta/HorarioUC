15034cf51045887e8316d4ff2764f7dc7ef87afc
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-07-14 22:40:58 -0400

    feat: formulario feedback

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,14 @@ function Footer() {
 
         </span>
         <a href="https://github.com/aurmeneta/HorarioUC">GitHub</a>
-
+        <br />
+        <span className="navbar-text w-100">
+          Si tienes algún comentario o sugerencia, por favor deja tu feedback en este
+          {' '}
+        </span>
+        <a href="https://forms.gle/WitMyxhgqR5BhZgMA" target="_blank" rel="noreferrer">formulario</a>
+        {' '}
+        o abre un Issue en GitHub.
         <br />
         <span className="w-100 navbar-text">{`v${process.env.npm_package_version}`}</span>
 
