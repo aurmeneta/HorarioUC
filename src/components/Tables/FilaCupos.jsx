b2a22613c0214828c7d253d7aed7a49a5939551b
Andrés Urmeneta Betzhold <aurmeneta@uc.cl>
2024-01-06 17:06:58 +0100

    feat: mostrar todas las categorías de cupos (#37)
    * feat: mostrar cupos completos
    * 1.6.0-rc.0

--- src/components/Tables/FilaCupos.jsx
+++ src/components/Tables/FilaCupos.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function FilaCupos({ cupo }) {
+function FilaCupos({ cupo, columnasIncluir }) {
   return (
     <tr>
       <td className="ps-3">{cupo.escuela}</td>
+      {columnasIncluir.nivel && <td>{cupo.nivel}</td>}
+      {columnasIncluir.programa && <td>{cupo.programa}</td>}
+      {columnasIncluir.concentracion && <td>{cupo.concentracion}</td>}
+      {columnasIncluir.cohorte && <td>{cupo.cohorte}</td>}
+      {columnasIncluir.admision && <td>{cupo.admision}</td>}
       <td>{cupo.vacantesOfrecidas}</td>
       <td>{cupo.vacantesOcupadas}</td>
       <th>{cupo.vacantesDisponibles}</th>
@@ -18,6 +23,18 @@ FilaCupos.propTypes = {
     vacantesDisponibles: PropTypes.number,
     vacantesOcupadas: PropTypes.number,
     vacantesOfrecidas: PropTypes.number,
+    nivel: PropTypes.string,
+    programa: PropTypes.string,
+    concentracion: PropTypes.string,
+    cohorte: PropTypes.string,
+    admision: PropTypes.string,
+  }).isRequired,
+  columnasIncluir: PropTypes.shape({
+    nivel: PropTypes.bool,
+    programa: PropTypes.bool,
+    concentracion: PropTypes.bool,
+    cohorte: PropTypes.bool,
+    admision: PropTypes.bool,
   }).isRequired,
 };
 
--- src/components/Tables/TablaCupos.jsx
+++ src/components/Tables/TablaCupos.jsx
@@ -5,19 +5,41 @@ import FilaCupos from './FilaCupos';
 
 function TablaCupos(props) {
   const { cupos } = props;
+
+  const nivel = cupos.cupos.some((cupo) => cupo.nivel !== '');
+  const programa = cupos.cupos.some((cupo) => cupo.programa !== '');
+  const concentracion = cupos.cupos.some((cupo) => cupo.concentracion !== '');
+  const cohorte = cupos.cupos.some((cupo) => cupo.cohorte !== '');
+  const admision = cupos.cupos.some((cupo) => cupo.admision !== '');
+
+  const columnasIncluir = {
+    nivel, programa, concentracion, cohorte, admision,
+  };
+
   return (
     <div className="table-responsive">
       <table className="table table-sm">
         <thead>
           <tr>
             <th className="ps-3">Escuela</th>
+            {nivel && <th>Nivel</th>}
+            {programa && <th>Programa</th>}
+            {concentracion && <th>Concentración</th>}
+            {cohorte && <th>Cohorte</th>}
+            {admision && <th>Admisión</th>}
             <th>Vacantes ofrecidas</th>
             <th>Vacantes ocupadas</th>
             <th>Vacantes libres</th>
           </tr>
         </thead>
         <tbody>
-          {cupos.cupos.map((cupo) => <FilaCupos cupo={cupo} key={cupo.escuela} />)}
+          {cupos.cupos.map((cupo) => (
+            <FilaCupos
+              cupo={cupo}
+              key={cupo.escuela}
+              columnasIncluir={columnasIncluir}
+            />
+          ))}
         </tbody>
       </table>
     </div>
@@ -35,6 +57,11 @@ TablaCupos.propTypes = {
         vacantesDisponibles: PropTypes.number,
         cupos: PropTypes.arrayOf(PropTypes.shape({
           escuela: PropTypes.string,
+          nivel: PropTypes.string,
+          programa: PropTypes.string,
+          concentracion: PropTypes.string,
+          cohorte: PropTypes.string,
+          admision: PropTypes.string,
           vacantesDisponibles: PropTypes.number,
           vacantesOcupadas: PropTypes.number,
           vacantesOfrecidas: PropTypes.number,
